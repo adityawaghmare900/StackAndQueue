59b125fffa229ef071bb68678c309d954283b716
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-18 21:00:10 +0530

    [aditya]: ability to create stack of element

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CreateStack_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(56);
+            linkedListStack.push(30);
+            linkedListStack.push(70);
+            Console.WriteLine("============================");
+            Console.WriteLine("elements are: ");
+            linkedListStack.display();
+        }
+    }
+}
